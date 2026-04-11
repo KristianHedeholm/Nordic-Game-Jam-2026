8cedf0b0e6efa80bb03cfc2cae67041b31c89691
Kristian Hedeholm <kristianh@rawpowerlabs.com>
2026-04-11 20:21:11 +0200

    Added Utilities

---
+++ Assets/Scripts/Diamond.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using RawPowerLabs.DynamicAI;
+using RawPowerLabs.DynamicAI.Utility;
+
+public class Diamond : MonoBehaviour
+{
+    [SerializeField]
+    private string _diamondName;
+    
+    private TextModule  _textModule;
+
+    private void Awake()
+    {
+	    var diamondPath = DiamondUtility.GetPathFromDiamondName(_diamondName);
+	    var context = new RawPowerLabs.DynamicAI.Context();
+	    var parameters = TextModuleParameters.GetDefault();
+	    _textModule = context.CreateTextModule(parameters, diamondPath.modelPath, diamondPath.templatePath);
+	    if (_textModule == null)
+	    {
+		    UnityEngine.Debug.LogError("Something went wrong with the Diamond");
+	    }
+    }
+}
---
+++ Assets/Scripts/Editor/DiamondEditorUtility.cs
@@ -0,0 +1,45 @@
+namespace RawPowerLabs.DynamicAI.Editor.Utility
+{
+	using System.IO;
+	using UnityEngine;
+	using UnityEditor;
+	using RawPowerLabs.DynamicAI.Utility;
+
+	public static class DiamondEditorUtility
+	{
+		private const string AssetFolder = "Assets";
+		private static bool DoesFolderWithDiamondsExist()
+		{
+			var diamondFolderPath = Path.Combine(Application.streamingAssetsPath, DiamondUtility.DiamondPath);
+			return Directory.Exists(diamondFolderPath);
+		}
+
+		[MenuItem("Raw Power Labs/Dynamic AI/Create Streaming Assets Diamond Folder", false)]
+		public static void CreateDiamondFolder()
+		{
+			var fullPath = Path.Combine(Application.streamingAssetsPath, DiamondUtility.DiamondPath);
+			var diamondFolderPath  = Path.GetRelativePath(AssetFolder, fullPath);
+			var diamondFolderPathParts = diamondFolderPath.Split(Path.DirectorySeparatorChar);
+			
+			var parent = AssetFolder;
+			for (int i = 0; i < diamondFolderPathParts.Length; i++)
+			{
+				var currentFolder = diamondFolderPathParts[i];
+				var currentPath = Path.Combine(parent, currentFolder);
+				
+				if(!AssetDatabase.IsValidFolder(currentPath))
+				{
+					AssetDatabase.CreateFolder(parent, currentFolder);
+				}
+
+				parent = currentPath;
+			}
+		}
+
+		[MenuItem("Raw Power Labs/Dynamic AI/Create Streaming Assets Diamond Folder", true)]
+		public static bool CheckIfDiamondFolderExist()
+		{
+			return !DoesFolderWithDiamondsExist();
+		}
+	}
+}
