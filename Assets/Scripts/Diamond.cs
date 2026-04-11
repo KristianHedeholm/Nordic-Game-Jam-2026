37afadee9d3a799fcb526b7b7d5f539c920efa0d
Kristian Hedeholm <kristianh@rawpowerlabs.com>
2026-04-11 22:48:35 +0200

    Added logic to trigger the diamond

--- Assets/Scripts/Diamond.cs
+++ Assets/Scripts/Diamond.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using RawPowerLabs.DynamicAI;
 using RawPowerLabs.DynamicAI.Utility;
+using Type = RawPowerLabs.DynamicAI.Type;
+using Color = RawPowerLabs.DynamicAI.Color;
+using Material  = RawPowerLabs.DynamicAI.Material;
 
 public class Diamond : MonoBehaviour
 {
@@ -21,5 +26,81 @@ public class Diamond : MonoBehaviour
 	    {
 		    UnityEngine.Debug.LogError("Something went wrong with the Diamond");
 	    }
+
+	    var types = Enum.GetValues(typeof(Type)) as Type[];
+	    var typeIndex = UnityEngine.Random.Range(0, types.Length);
+	    var randomType = types[typeIndex].ToString();
+	    UnityEngine.Debug.Log(randomType);
+	    
+	    var colors = Enum.GetValues(typeof(Color)) as Color[];
+	    var colorIndex = UnityEngine.Random.Range(0, colors.Length);
+	    var randomColor = colors[colorIndex].ToString();
+	    
+	    var materials = Enum.GetValues(typeof(Material)) as Material[];
+	    var materialIndex = UnityEngine.Random.Range(0, materials.Length);
+	    var randomMaterial = materials[materialIndex].ToString();
+	    
+	    PrintReplies(randomType, randomColor, randomMaterial);
     }
+    
+    private async void PrintReplies(string type, string color, string material)
+    {
+	    var replies = new Dictionary<string, string>();
+
+	    try
+	    {
+		    replies = await InvokeReplyAsync(type, color, material);
+	    }
+	    catch (Exception e)
+	    {
+		    UnityEngine.Debug.LogException(e);
+	    }
+
+	    foreach (var reply in replies)
+	    {
+		    UnityEngine.Debug.Log($"Key: {reply.Key}, Value: {reply.Value}");
+	    }
+    }
+    
+    private async Task<Dictionary<string, string>> InvokeReplyAsync(string type, string color, string material)
+	{
+		return await Task.Run(() => InvokeReply(type, color, material));
+	}
+	
+	private Dictionary<string, string> InvokeReply(string type, string color, string material)
+	{
+		if (_textModule == null)
+		{
+			UnityEngine.Debug.LogError("_textModule is null");
+			return null;
+		}
+		
+		using var textModuleInput = _textModule.CreateInput();
+
+		textModuleInput.Set("Type", type);
+		
+		var colorCategory = CategoricalInputCollection.TypeNames[typeof(Color)];
+		textModuleInput.Set(colorCategory, color);
+		
+		var materialCategory = CategoricalInputCollection.TypeNames[typeof(Material)];
+		textModuleInput.Set(materialCategory, material);
+		
+		
+		var invokeParameters = TextModuleInvokeParameters.GetDefault();
+		invokeParameters.PredictCount = 4048;
+
+		var random = new System.Random();
+		invokeParameters.Seed = (uint) random.Next(0, int.MaxValue);
+		using var textResult = _textModule.Invoke(invokeParameters, textModuleInput);
+		
+		var replies = new Dictionary<string, string>();
+		
+		foreach (var outputValues in CategoricalOutputCollection.StringOutputValues)
+		{
+			var result = textResult.GetString(outputValues.Value);
+			replies.Add(outputValues.Value, result);
+		}
+	    
+		return replies;
+	}
 }
