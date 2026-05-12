ecaff3c118b59ec54fc0ca10ece7dabf38b4e788
Kristian Hedeholm <kristianh@rawpowerlabs.com>
2026-05-12 10:39:19 +0200

    Added logging for the riddles.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Text;
 using RawPowerLabs.DynamicAI;
 using UnityEngine;
 
@@ -62,6 +64,7 @@ public class GameManager : MonoBehaviour
                 FetchRiddleAndShow(RiddleKind.Material, GamePhase.Reveal);
                 break;
             case GamePhase.Reveal:
+                LogRiddles(RiddleDataCollection);
                 uiManager.ShowReveal(RiddleDataCollection);
                 break;
             case GamePhase.FinalJudgment:
@@ -105,5 +108,35 @@ public class GameManager : MonoBehaviour
         await diamond.GenerateRiddles(RiddleDataCollection);
         
         GoToPhase(GamePhase.GuessClothing);
+    }
+
+    private static void LogRiddles(RiddleDataCollection riddleDataCollection)
+    {
+	    var dataPath = Application.persistentDataPath;
+	    var fileName = "riddleLog.txt";
+	    var fullPath = Path.Combine(dataPath, fileName);
+
+	    if (!File.Exists(fullPath))
+	    {
+		    File.WriteAllText(fullPath, string.Empty);
+	    }
+	    
+	    var currentLog = File.ReadAllText(fullPath);
+	    var stringBuilder = new StringBuilder(currentLog);
+	    var enumValues = Enum.GetValues(typeof(RiddleKind)) as  RiddleKind[];
+	    
+	    stringBuilder.AppendLine("New Game of Riddles");
+	    foreach (var enumValue in enumValues)
+	    {
+		    var riddleData = riddleDataCollection.GetRiddleData(enumValue);
+		    var riddleDataLog = $"Kind: {enumValue}," +
+		                        $" Riddle: '{riddleData.Riddle}'," +
+		                        $" Answers: {string.Join(", ", riddleData.Answers.AvailableAnswers)}," +
+		                        $" Correct Answer: '{riddleData.Answers.CorrectAnswer}'," +
+		                        $" Guessed Answer: '{riddleData.GuessedAnswer}',";
+		    stringBuilder.AppendLine(riddleDataLog);
+	    }
+	    
+	    File.WriteAllText(fullPath, stringBuilder.ToString());
     }
 }
--- Assets/Scripts/RiddleDataCollection.cs
+++ Assets/Scripts/RiddleDataCollection.cs
@@ -103,4 +103,9 @@ public class RiddleDataCollection
 	    }
 	    return true;
     }
+
+    public RiddleData GetRiddleData(RiddleKind riddleKind)
+    {
+	    return _riddleData[riddleKind];
+    }
 }
